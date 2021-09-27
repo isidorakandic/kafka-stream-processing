6f8baf74733d22219cc9c5f5c301a0ea0501331c
Isidora Kandic <end77875@adobe.com>
2021-09-27 02:31:28 +0200

    Agreggation using HashSet implemented

--- src/main/java/com/isidora/kafka/UniqueUsersCounter.java
+++ src/main/java/com/isidora/kafka/UniqueUsersCounter.java
@@ -1,15 +1,18 @@
 package com.isidora.kafka;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.lang3.SerializationUtils;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.connect.json.JsonDeserializer;
 import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
 
 import java.time.Duration;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 public class UniqueUsersCounter {
@@ -17,6 +20,27 @@ public class UniqueUsersCounter {
     static public final class JsonSerde extends Serdes.WrapperSerde<JsonNode> {
         public JsonSerde() {
             super(new JsonSerializer(), new JsonDeserializer());
+        }
+    }
+
+    static public final class HashSetSerde extends Serdes.WrapperSerde<HashSet<String>> {
+        public HashSetSerde() {
+            super(new HashSetSerializer(), new HashSetDeserializer());
+        }
+    }
+
+    static final class HashSetSerializer implements Serializer<HashSet<String>> {
+        @Override
+        public byte[] serialize(String topic, HashSet<String> strings) {
+            return SerializationUtils.serialize(strings);
+        }
+    }
+
+    static final class HashSetDeserializer implements Deserializer<HashSet<String>> {
+
+        @Override
+        public HashSet<String> deserialize(String topic, byte[] bytes) {
+            return SerializationUtils.deserialize(bytes);
         }
     }
 
@@ -34,6 +58,7 @@ public class UniqueUsersCounter {
 
         final StreamsBuilder builder = new StreamsBuilder();
         final Serde<JsonNode> jsonSerde = new JsonSerde();
+        final Serde<HashSet<String>> hashSetSerde = new HashSetSerde();
         streamsProperties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         KStream<String, JsonNode> source = builder.stream(inputTopic, Consumed.with(Serdes.String(), jsonSerde));
         // this results in this:
@@ -46,26 +71,26 @@ public class UniqueUsersCounter {
         // key: null value: "b7e4ef7a91f18c0f3d1"
 
 
-        KGroupedStream<String, String> groupedStream = sourceIDs.groupBy((key, value) -> value);
-        // grouped is done so we get KGroupedStream object on which we can all windowing. we group by uid so we can get unique users (questionable logic since we may be losing this property during further transformations)
+        KGroupedStream<String, String> groupedStream = sourceIDs.groupBy((key, value) -> "");
+        // grouped is done so we get KGroupedStream object on which we can all windowing. grouping is done to the same key, so we don't get substreams we don't need
 
-        TimeWindowedKStream<String, String> windowedStream = groupedStream.windowedBy(TimeWindows.of(Duration.ofMinutes(1)));
+        TimeWindowedKStream<String, String> windowedStream = groupedStream.windowedBy(TimeWindows.of(Duration.ofMinutes(1)).advanceBy(Duration.ofSeconds(60)));
         // this is done to get partitions of input data by the minute (time used for this evaluation is the time the event entered the input stream)
 
-        KTable<Windowed<String>, String> aggregated = windowedStream.aggregate(() -> "", (aggKey, newValue, aggValue) -> "");
-        // aggregate only provides us with timestamp and uid in the key and gives us a KTable object
+        KTable<Windowed<String>, HashSet<String>> aggregated = windowedStream.aggregate(HashSet::new,
+                (aggKey, newValue, aggValue) -> {
+                    aggValue.add(newValue);
+                    return aggValue;
+                }, Materialized.with(Serdes.String(), hashSetSerde));
+        // timestamp is the key. for each key, we keep a record of user ids associated with it in a set.
+        // this gives a list (in form of a set so we don't need to worry about duplicates) of all user ids that accessed the system at that time
 
-        KStream<String, String> mapTsId = aggregated.toStream().map((key, value) -> new KeyValue<>(key.window().startTime().toString(), value));
-        // finally, we get a stream that looks like this:
-        // key: timestamp value: uid
+        KTable<Windowed<String>, Integer> windowedUniqueUsers = aggregated.mapValues(HashSet::size);
+        // number of unique users per timestamp (in this case, minute) is simply the size of the set
 
-        KTable<String, Long> uniqueUsersPerMinute = mapTsId.groupByKey().count();
-        // grouping by timestamp and counting the number of values associated with each key gives us the number of unique users per timestamp (minute)
-
-        KStream<String, String> finalResult = uniqueUsersPerMinute.toStream().map((key, value) -> new KeyValue<>(key, key + " " + value));
-        // value field is enriched with timestamp so timestamp can be visible when consuming the data from the output topic in kafka directly
-
-        finalResult.to(outputTopic);
+        KStream<String, Integer> uniqueUsers = windowedUniqueUsers.toStream().map((key, value) -> new KeyValue<>(key.window().startTime().toString(), value));
+        // this is done so the key is the timestamp of the beginning of the minute
+        uniqueUsers.to(outputTopic);
 
         final Topology topology = builder.build();
         System.out.println(topology.describe()); // to see what kind of topology is created
